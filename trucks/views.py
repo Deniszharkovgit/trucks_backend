4c49f86e10f39251703ba188eabadb351a723b97
Denis Zharkov <denisjs4@mail.ru>
2023-03-06 22:25:08 +0300

    fix

--- trucks/views.py
+++ trucks/views.py
@@ -7,11 +7,9 @@ from trucks.models import Truck, TruckModel
 def trucks_page(request):
     context = {
         'models': TruckModel.objects.all(),
-        'trucks': Truck.objects.all()
     }
     if request.GET.get('featured') and request.GET.get('featured') != 'all':
-        trucks = Truck.objects.filter(truck_model=request.GET.get('featured'))
-        context['trucks'] = trucks
+        context['trucks'] = Truck.objects.filter(truck_model=request.GET.get('featured'))
     else:
         context['trucks'] = Truck.objects.all()
     return render(request, "../templates/trucks/truck_list.html", context)
