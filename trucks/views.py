b279da97808ccbb0469a1646e914f8d694e382c7
Denis Zharkov <denisjs4@mail.ru>
2023-03-14 13:07:30 +0300

    upd model and view, protect sql-injection

--- trucks/views.py
+++ trucks/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from trucks.models import Truck, TruckModel
 
 
 class MainView(View):
+    template_name = "../templates/trucks/truck_list.html"
+
     def get(self, request):
+        featured = request.GET.get('featured')
+        trucks = Truck.objects.all()
+        models = TruckModel.objects.all()
+        prev_value = featured or 'all'
+
+        if featured and featured != 'all':
+            truck_model = get_object_or_404(TruckModel, name=featured)
+            trucks = Truck.objects.filter(truck_model=truck_model)
+
         context = {
-            'models': TruckModel.objects.all(),
-            'prev_value': request.GET.get('featured'),
+            'models': models,
+            'prev_value': prev_value,
+            'trucks': trucks,
         }
-        if request.GET.get('featured') and request.GET.get('featured') != 'all':
-            context['trucks'] = Truck.objects.filter(truck_model=request.GET.get('featured'))
-        else:
-            context['trucks'] = Truck.objects.all()
-        return render(request, "../templates/trucks/truck_list.html", context)
+
+        return render(request, self.template_name, context)
